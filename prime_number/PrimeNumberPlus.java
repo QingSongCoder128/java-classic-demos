f49f244ace0ce93144d3550de2b3de336999af60
QingSongCoder128 <caoqingsong2025@outlook.com>
2026-03-13 16:22:09 +0800

    Update PrimeNumberPlus.java
    控制每行输出五个素数显示

--- prime_number/PrimeNumberPlus.java
+++ prime_number/PrimeNumberPlus.java
@@ -1,8 +1,9 @@
 public class PrimeNumberPlus {
     public static void main(String[] args) {
+        int count = 0;//我们定义一个变量来计数，为了每行输出五个，然后换行
         System.out.println("100以内的素数：");
         for (int i = 2; i <= 100; i++) {  // 素数从2开始
-            boolean isPrime = true;
+            boolean isPrime = true;//归零，每次判断下一个数开始，flag恢复为初始值
             for (int j = 2; j < i; j++) {  // 检查是否有因子
                 if (i % j == 0) {
                     isPrime = false;
@@ -10,7 +11,11 @@ public class PrimeNumberPlus {
                 }
             }
             if (isPrime) {
-                System.out.print(i+"\t");
+                System.out.print(i + "\t");
+                count++;
+                if (count % 5 == 0) {
+                    System.out.println();
+                }
             }
         }
     }
