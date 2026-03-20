82651646d80e2790bb4d3ca74f4e87be024c5465
QingSongCoder128 <caoqingsong2025@outlook.com>
2026-03-20 17:01:43 +0800

    Update SelectionSort.java

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         //排序前
         System.out.println(Arrays.toString(a));
         for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i; j < a.length; j++) {
+            for (int j = i+1; j < a.length; j++) {
                 if (a[i] > a[j]) {
                     int temp = a[i];
                     a[i] = a[j];
