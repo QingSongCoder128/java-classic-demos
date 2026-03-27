6b7efb72a81de2f2250471b801f42ec6700618e7
QingSongCoder128 <caoqingsong2025@outlook.com>
2026-03-27 15:09:40 +0800

    Update FibonacciSequence03.java
    使用递归算法和异常来实现斐波拉契数列

--- fibonacci_sequeece/FibonacciSequence03.java
+++ fibonacci_sequeece/FibonacciSequence03.java
@@ -1,17 +1,25 @@
 public class FibonacciSequence03 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入你要计算哪一项：");
-        int n = scanner.nextInt();
-        int num = GetNum(n);
-        System.out.println(num);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入你要计算哪一项：");
+            int n = scanner.nextInt();
+            int num = GetNum(n);
+            System.out.println(num);
+        } catch (Exception e) {
+            System.out.println("请输入正整数，重新输入");
+            throw new RuntimeException(e);
+        }
     }
 
     public static int GetNum(int a) {
         if (a == 1 || a == 2) {
             return 1;
+        } else if (a > 2) {
+            return GetNum(a - 1) + GetNum(a - 2);
         } else {
-            return GetNum(a-1) + GetNum(a - 2);
+            throw new RuntimeException();
         }
     }
 }
+
